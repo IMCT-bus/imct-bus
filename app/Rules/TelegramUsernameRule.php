45fe172060a4d93fb22932887a0fb05b3fd3650d
travka47 <travka.eal@students.dvfu.ru>
2023-11-13 00:50:34 +1000

    minor: add Registration request / BUS-2

---
+++ app/Rules/TelegramUsernameRule.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Rules;
+
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
+
+/**
+ * Поле может содержать символы a-z, 0-9 и нижнее подчёркивание.
+ * Минимальная длина 5 символов, максимальная - 32.
+ */
+class TelegramUsernameRule implements ValidationRule
+{
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        if (!preg_match('/^[a-z0-9_]{5,32}$/', $value)) {
+            $fail('Невалидный telegram, поле может содержать символы a-z, 0-9 и _.');
+        }
+    }
+}
