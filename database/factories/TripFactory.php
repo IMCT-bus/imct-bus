e41fb226e92c78a2879e68abfd23ab7898245f70
travka47 <travka.eal@students.dvfu.ru>
2023-11-13 19:27:16 +1000

    minor: 0.5 chance of getting null for car_number and link in Trip Factory

--- database/factories/TripFactory.php
+++ database/factories/TripFactory.php
@@ -9,13 +9,16 @@ class TripFactory extends Factory
 {
     public function definition(): array
     {
+        $carNumber = random_int(1, 100) <= 50 ? null : 'А ' . random_int(111, 999) . ' БВ';
+        $link = random_int(1, 100) <= 50 ? null : fake()->url();
+
         return [
             'uuid' => fake()->uuid(),
             'date' => fake()->date(),
             'route_id' => fake()->randomElement(Route::query()->pluck('id')),
             'is_published' => fake()->boolean(60),
-            'car_number' => 'А 123 БВ',
-            'link' => fake()->url(),
+            'car_number' => $carNumber,
+            'link' => $link,
             'seats' => fake()->numberBetween(10, 40),
         ];
     }
