3ba59ed3762bf1ef9bbf05c5675cf659bd8661d3
travka47 <travka.eal@students.dvfu.ru>
2023-11-27 21:25:19 +1000

    fix: TelegramUsernameRule

--- app/Rules/TelegramUsernameRule.php
+++ app/Rules/TelegramUsernameRule.php
@@ -6,15 +6,23 @@ use Closure;
 use Illuminate\Contracts\Validation\ValidationRule;
 
 /**
- * Поле может содержать символы a-z, 0-9 и нижнее подчёркивание.
+ * Поле может содержать символы A-z, 0-9 и нижнее подчёркивание.
  * Минимальная длина 5 символов, максимальная - 32.
  */
 class TelegramUsernameRule implements ValidationRule
 {
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
-        if (!preg_match('/^[A-za-z0-9_]{5,32}$/', $value)) {
-            $fail('Невалидный telegram.');
+        if (str_contains($value, 't.me/')) {
+            $fail("Введите telegram username, не ссылку.");
+        }
+
+        if (str_contains($value, '@')) {
+            $fail("Telegram username не должен содержать символ @.");
+        }
+
+        if (!preg_match( '/^[a-zA-Z0-9_]+$/', $value)) {
+            $fail('Невалидный telegram username.');
         }
     }
 }
