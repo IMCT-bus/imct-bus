5054e4781e6bd0bf9e2a455444e994ea9f3e1840
fiftysev <nesgorai@bk.ru>
2023-11-27 23:37:24 +1000

    chore: favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,9 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link type="image/png" sizes="16x16" rel="icon" href="{{asset('icons8-bus-papercut-16.png')}}">
+    <link type="image/png" sizes="32x32" rel="icon" href="{{asset('icons8-bus-papercut-32.png')}}">
+    <link type="image/png" sizes="96x96" rel="icon" href="{{asset('icons8-bus-papercut-96.png')}}">
 
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
